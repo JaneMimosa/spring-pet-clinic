d38639a9cc10a6137befad826ea928fe9b27788e
Jane <jane666bender@gmail.com>
2023-01-20 22:14:18 +0300

    Initial commit

---
+++ pet-clinic-web/src/main/java/com/springframework/SpringPetClinicApplication.java
@@ -0,0 +1,15 @@
+package com.springframework;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		run(SpringPetClinicApplication.class, args);
+	}
+
+}

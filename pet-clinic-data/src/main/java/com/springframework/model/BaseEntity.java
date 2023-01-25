fc9724fffd315917dab84a8e1d03b2530e5d7b88
Jane <jane666bender@gmail.com>
2023-01-25 10:59:24 +0300

    fixes

--- pet-clinic-data/src/main/java/com/springframework/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/model/BaseEntity.java
@@ -5,12 +5,6 @@ import java.io.Serializable;
 public class BaseEntity implements Serializable {
     private Long id;
 
-    public BaseEntity() {
-    }
-    public BaseEntity(Long id) {
-        this.id = id;
-    }
-
     public Long getId() {
         return id;
     }
--- pet-clinic-web/src/main/java/com/springframework/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/SpringPetClinicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static org.springframework.boot.SpringApplication.run;
 
 
-@SpringBootApplication()
+@SpringBootApplication
 public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {

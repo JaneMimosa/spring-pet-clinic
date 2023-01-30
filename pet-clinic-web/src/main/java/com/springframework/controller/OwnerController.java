2e4964972c4b59c16b1dca2a798656e8d74df58e
JaneMimosa <jane666bender@gmail.com>
2023-01-30 23:12:44 +0300

    Fixes

--- pet-clinic-web/src/main/java/com/springframework/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/controller/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
 
         return "/owners/index.html";
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }

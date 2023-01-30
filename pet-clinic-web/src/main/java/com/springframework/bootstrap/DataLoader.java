12d664ae5b35c6ee9ab8f5944608ca4e6e32b660
JaneMimosa <jane666bender@gmail.com>
2023-01-30 22:05:14 +0300

    Added PetTypes to bootstrap

--- pet-clinic-web/src/main/java/com/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/bootstrap/DataLoader.java
@@ -10,14 +10,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes....");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");

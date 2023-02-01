147509caa30c05dac64f832209df6e94acb9640d
JaneMimosa <jane666bender@gmail.com>
2023-02-01 21:45:43 +0300

    Added Spring Data Jpa Service for Specialties

---
+++ pet-clinic-data/src/main/java/com/springframework/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package com.springframework.services.springdatajpa;
+
+import com.springframework.model.Speciality;
+import com.springframework.repositories.SpecialtyRepository;
+import com.springframework.services.SpecialtiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtiesService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}

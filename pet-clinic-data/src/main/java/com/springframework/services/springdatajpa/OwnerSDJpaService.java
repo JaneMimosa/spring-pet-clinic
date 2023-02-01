765f12716151bd3851009f514ba55940e14a2f05
JaneMimosa <jane666bender@gmail.com>
2023-02-01 22:02:58 +0300

    Added Profile for Owner SDJPpa Service

--- pet-clinic-data/src/main/java/com/springframework/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/springframework/services/springdatajpa/OwnerSDJpaService.java
@@ -3,12 +3,14 @@ package com.springframework.services.springdatajpa;
 import com.springframework.model.Owner;
 import com.springframework.repositories.OwnerRepository;
 import com.springframework.services.OwnerService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
